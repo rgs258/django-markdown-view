742ad979b7d15dca5dad51a3e65955d68a386699
Timothy Allen <flipper@peregrinesalon.com>
2022-03-30 07:14:37 -0400

    Bump version.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_desc = open('README.rst', 'rb').read().decode('utf-8') + '\n\n' + \
 
 setup(
     name='django-markdown-view',
-    version='0.0.4',
+    version='0.0.5',
     description='Serve .md pages as Django views.',
     long_description=long_desc,
     author='Ryan J. Sullivan',
